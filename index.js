ff07fb916fde322a30ecb138b1ba08f5bc517dfa
nba <nbadigitalservice@gmail.com>
2025-04-02 15:22:47 +0700

    first commit

--- index.js
+++ index.js
@@ -38,17 +38,21 @@ const postType = require('./routes/Post/post.type')
 const recordType = require('./routes/Record/record.type')
 const recordFlow = require('./routes/Record/record.flow')
 const invoice = require('./routes/invoice')
+const deposit = require('./routes/Deposit');
 
 app.use(bodyParser.json({ limit: '50mb', type: 'application/json' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.set("strictQuery", true);
 mongoose.connect(process.env.DB).then(() => console.log('Connected!'));
 const cors = require("cors");
+const { createDeposit } = require("./controllers/deposit.controller");
+const depositRoutes = require('./routes/deposit/deposit.routes');
 
 app.use(express.json());
 app.use(cors());
 app.use('/ddsc-office/uploads/picture', express.static(path.join(__dirname, 'uploads/picture')));
 app.use('/ddsc-office/uploads/pdf', express.static(path.join(__dirname, 'uploads/pdf')));
+app.use('/ddsc-office', depositRoutes);
 
 const ddsc = '/ddsc-office'
 // เรียกใช้ Path ตรงนี้ <=
@@ -92,9 +96,11 @@ app.use(ddsc + '/exam', ExamRoute);
 app.use(ddsc + '/exam-type', ExamTypeRoutes);
 app.use(ddsc + '/examresults', ExamResultsRoutes);
 app.use(ddsc + '/contact', ContactRoutes)
-
+/////////////////////////////////////////////////////////
 app.use(ddsc + '/invoice', invoice)
-
+//app.use(ddsc + '/deposit',deposit)
+//app.use('/ddsc-office', depositRoutes);
+//////////////////////////////////////////////////////////
 // รับงาน
 app.use(ddsc + '/project', RequestProjectRoutes);
 app.use(ddsc + '/project-types', ProjectTypeRoutes);
--- routes/deposit.js
+++ routes/deposit.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const depositController = require('../controllers/deposit.controller');
 
-router.post('/ddsc-office/deposit', depositController.createDeposit);
+router.post('/deposit', depositController.createDeposit);
 
 module.exports = router;
